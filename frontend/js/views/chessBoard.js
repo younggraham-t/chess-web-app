885125392b5bda4514b451128ee17dea71b17743
younggraham-t <younggraham.t@gmail.com>
2023-10-26 13:06:13 -0400

    add dynamic resizing with aspect ratio to board

--- frontend/js/views/chessBoard.js
+++ frontend/js/views/chessBoard.js
@@ -1,20 +1,16 @@
-const boardSize = window.innerHeight > 850 ? 640 : 448; //640 (divides evenly by 8) is 75% of 850 so if the screen is smaller use a smaller board
+const boardSize = window.innerHeight > 850 ? 640 : 488; //640 (divides evenly by 8) is 75% of 850 so if the screen is smaller use a smaller board
 const numRowsAndCols = 8;
 const svgns = "http://www.w3.org/2000/svg";
 
 const startingPositionFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 // console.log(window.innerHeight);
 // console.log(window.innerWidth);
-// import { parseFen } from 'fentastic';
-// console.log(parseFen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'));
 
 
 generateBoard()
 generatePosition(startingPositionFen);
 // generatePosition('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2');
 
-generatePosition(startingPositionFen);
-// generatePosition('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2');
 
 let selectedPiece = null;
 
@@ -189,37 +185,42 @@ function buildBoard() {
     let boardAttrArray = [buildAttributeObject("class", "board")];
     // generate the board element
     let board = buildNewSVGElement("svg", boardAttrArray);
-    board.setAttribute("preserveAspectRatio", "xMinYMin meet")
-    board.setAttribute("viewBox", "0 0 " + boardSize + " " + boardSize)
+    board.setAttribute("preserveAspectRatio", "xMinYMax meet")
+    // board.setAttribute("viewBox", "0 0 " + boardSize + " " + boardSize)
     // let board = buildNewElement("div", boardAttrArray);
     // buildNewSVGElement("image", [buildAttributeObject("xlink:href", "../assets/images/WK.png"), buildAttributeObject("x", "0"), buildAttributeObject("y", "0"), buildAttributeObject("width", "100"), buildAttributeObject("height", "100")]);
     
     //generate and add the squares
+    let locationX = 1;
+    let locationY = 8
     let isLightSquare = true;
     for (x=0; x<numRowsAndCols; x++) {
         for (y=0; y<numRowsAndCols; y++) {
-            xPos = x * boardSize/numRowsAndCols;
-            yPos = y * boardSize/numRowsAndCols;
-            let newSqaure = generateSquare(xPos, yPos, isLightSquare);
+            yPos = x * 12.5;
+            xPos = y * 12.5;
+            let newSqaure = generateSquare(xPos, yPos, isLightSquare, locationX + "" + locationY);
             // newSqaure.style.backgroundImage = "url(data:image/svg+xml;utf8," + svgCode + ")";
    
             board.appendChild(newSqaure);
             isLightSquare = !isLightSquare
+            locationX++;
         }
         isLightSquare = !isLightSquare
+        locationY--;
+        locationX = 1;
     }
     return board;
 }
 function generateSquare(x, y, isLight, locationId) {
-    let xAttr = buildAttributeObject("x", x);
-    let yAttr = buildAttributeObject("y", y);
-    let classAttr = buildAttributeObject("class", isLight ? "light-square" : "dark-square");
+    let xAttr = buildAttributeObject("x", x + "%");
+    let yAttr = buildAttributeObject("y", y + "%");
+    let classAttr = buildAttributeObject("class", isLight ? "light square-": "dark square-" );
     // let classAttr = buildAttributeObject("class", isLight ? "piece br " + locationId : "piece wr " + locationId);
-    let heightAttr = buildAttributeObject("height", boardSize/numRowsAndCols);
-    let widthAttr = buildAttributeObject("width", boardSize/numRowsAndCols);
-    let idAttr = buildAttributeObject("id", locationId) 
+    let heightAttr = buildAttributeObject("height", "12.5%");
+    let widthAttr = buildAttributeObject("width", "12.5%");
+    // let idAttr = buildAttributeObject("id", locationId) 
     
-    let attrArray = [xAttr, yAttr, classAttr, heightAttr, widthAttr, idAttr];
+    let attrArray = [xAttr, yAttr, classAttr, heightAttr, widthAttr];
     // let attrArray = [xAttr, yAttr, classAttr, idAttr];
     let newSqaure = buildNewSVGElement("rect", attrArray);
     // let newSqaure = buildNewElement("div", attrArray);
